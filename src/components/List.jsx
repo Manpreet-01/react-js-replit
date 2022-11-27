ef6ce68bff9490ad7bcf2110782371e48d895337
Manpreet Singh <manpreetmss400@gmail.com>
2022-11-27 09:16:45 +0000

    * as export

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
-import pi, {doublepi, triplepi} from './math'
+import * as pi from './math'
 
 function List() {
   return (
     <ol>
-      <li>{pi}</li>
-      <li>{pi}</li>
-      <li>600</li>
-      <li>{ doublepi() }</li>
+      <li>{pi.default}</li>
+      <li>{pi.doublepi()}</li>
+      <li>{pi.triplepi()}</li>
     </ol>
   );
 }
