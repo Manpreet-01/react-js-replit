74aca737ac52a6d00a4d41d824ceabcd21ed8168
Manpreet Singh <>
2022-11-27 08:28:21 +0000

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import Heading from './Heading'
+import List from './List'
+
+// import './App.css'
+
+export default function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
---
+++ src/components/List.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function List() {
+  return (
+    <ol>
+      <li>Banana Shake</li>
+      <li>Milk Shake</li>
+      <li>Dry Fruits</li>
+    </ol>
+  );
+}
+
+export default List;
